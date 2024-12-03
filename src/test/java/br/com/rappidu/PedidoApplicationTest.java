68b84e7ef7cbc65d9a3645367acbd6591b5a7219
raylanef <raylanefernandes@hotmail.com>
2024-12-02 23:00:55 -0300

    add teste

---
+++ src/test/java/br/com/rappidu/PedidoApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.rappidu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PedidoApplicationTest {
+  @Test
+    void testeTeste(){
+      var x = 0;
+      Assertions.assertTrue(x==0);
+  }
+}
